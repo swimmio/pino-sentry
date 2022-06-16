a1a81fc3d8a775b1560b45c0435bf0f433bc5dd0
Segev Finer <segev@swimm.io>
2022-06-16 22:58:27 +0300

    Remove unnecessary withScope change

--- src/transport.ts
+++ src/transport.ts
@@ -122,39 +122,38 @@ export class PinoSentryTransport {
         const message = get(chunk, this.messageAttributeKey);
         const stack = get(chunk, this.stackAttributeKey) || '';
 
-        Sentry.withScope((scope) => {
-          this.decorateScope(chunk, scope);
+        const scope = new Sentry.Scope();
+        this.decorateScope(chunk, scope);
 
-          scope.setLevel(severity);
+        scope.setLevel(severity);
 
-          if (this.isObject(tags)) {
-            Object.keys(tags).forEach(tag => scope.setTag(tag, tags[tag]));
-          }
+        if (this.isObject(tags)) {
+          Object.keys(tags).forEach(tag => scope.setTag(tag, tags[tag]));
+        }
 
-          if (this.isObject(extra)) {
-            Object.keys(extra).forEach(ext => scope.setExtra(ext, extra[ext]));
-          }
+        if (this.isObject(extra)) {
+          Object.keys(extra).forEach(ext => scope.setExtra(ext, extra[ext]));
+        }
 
-          if (this.isObject(breadcrumbs)) {
-            Object.values(breadcrumbs).forEach(breadcrumb => scope.addBreadcrumb(breadcrumb));
-          }
+        if (this.isObject(breadcrumbs)) {
+          Object.values(breadcrumbs).forEach(breadcrumb => scope.addBreadcrumb(breadcrumb));
+        }
 
-          // Capturing Errors / Exceptions
-          if (this.isSentryException(severity)) {
-            const error = message instanceof Error ? message : new ExtendedError({ message, stack });
+        // Capturing Errors / Exceptions
+        if (this.isSentryException(severity)) {
+          const error = message instanceof Error ? message : new ExtendedError({ message, stack });
 
-            setImmediate(() => {
-              Sentry.captureException(error, scope);
-              cb();
-            });
-          } else {
-            // Capturing Messages
-            setImmediate(() => {
-              Sentry.captureMessage(message, scope);
-              cb();
-            });
-          }
-        });
+          setImmediate(() => {
+            Sentry.captureException(error, scope);
+            cb();
+          });
+        } else {
+          // Capturing Messages
+          setImmediate(() => {
+            Sentry.captureMessage(message, scope);
+            cb();
+          });
+        }
       });
     });
   }
